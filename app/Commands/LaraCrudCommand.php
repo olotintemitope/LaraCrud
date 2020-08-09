c6238ac5123f70b03895a7c9a8cfd49a3e6e9a82
Temitope Olotin <olotintemitope@gmail.com>
2020-08-09 07:51:24 +0100

    get column field types

--- app/Commands/LaraCrudCommand.php
+++ app/Commands/LaraCrudCommand.php
@@ -2,11 +2,12 @@
 
 namespace App\Commands;
 
+use App\Contracts\ConstantInterface;
 use App\Services\FileWriter;
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
 
-class LaraCrudCommand extends Command
+class LaraCrudCommand extends Command implements ConstantInterface
 {
     /**
      * The signature of the command.
@@ -39,17 +40,45 @@ class LaraCrudCommand extends Command
             $this->error("Name argument is missing");
         }
 
-        $defaultModelDirectory = $writer::getDefaultModelDirectory($modelDirectory, $applicationNamespace);
+        while (true) {
+            $dbColumnName = $this->ask('Enter column field name');
+            if (empty($dbColumnName)) {
+                $this->error("column field name is missing");
+            }
 
-        if (!empty($modelName)) {
-            $capitalizedModelNamespace = str_replace('/', '\\', $defaultModelDirectory);
+            if (!empty($dbColumnName)) {
+                $dbColumnFieldType = $this->choice(
+                    'Select column field type',
+                    array_keys(static::AVAILABLE_COLUMN_TYPES),
+                    $defaultIndex = 38,
+                    $maxAttempts = null,
+                    $allowMultipleSelections = false
+                );
 
-            $content = "<?php \n\r namespace {$capitalizedModelNamespace}; \n\r use Illuminate\Database\Eloquent\Model; \n\r class {$modelName} extends Model \n{\n\r}";
+                $exit = $this->ask('Are you done?');
+
+
+                // Cater for enum types. $table->enum('level', ['easy', 'hard']);
+                // Cater for float or decimal $table->decimal('amount', 8, 2);
+                /**
+                 * [
+                 *   first_name => [string],
+                 *   state => [enum => [ado ekiti, owo, ibadan]
+                 * ]
+                 */
+
+            }
         }
 
-        $this->question('Add Available Column Types');
-
         try {
+            $defaultModelDirectory = $writer::getDefaultModelDirectory($modelDirectory, $applicationNamespace);
+
+            if (!empty($modelName)) {
+                $capitalizedModelNamespace = str_replace('/', '\\', $defaultModelDirectory);
+
+                $content = "<?php \n\r namespace {$capitalizedModelNamespace}; \n\r use Illuminate\Database\Eloquent\Model; \n\r class {$modelName} extends Model \n{\n\r}";
+            }
+
             $modelPath = $writer::getModelWorkingDirectory($defaultModelDirectory, $modelName);
             $writer::write($defaultModelDirectory, $modelPath, $content);
             $this->info("{$modelName} was created for you and copied to the {$defaultModelDirectory} folder");
---
+++ app/Entities/User.php
@@ -0,0 +1,6 @@
+<?php 
+ namespace App\Entities; 
+ use Illuminate\Database\Eloquent\Model; 
+ class User extends Model 
+{
+}
